3b2922600b700dc55812a4436a23cb0817443f0c
CristianDevk <lucristian.oliveira2007@gamil.com>
2024-04-15 17:54:10 -0300

    Upload Files

---
+++ src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+import Example from "../components/Badge";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Example />
+        </>
+    )
+}
